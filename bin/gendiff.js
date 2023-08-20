3e6662cc94fb39da274eef7e60efbdf63344f806
Kseniia Faizulina <ksyushafaizulina@icloud.com>
2023-08-20 21:46:54 +0700

    Update gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,6 +3,7 @@
 import { program } from 'commander';
 
 program
+  .name('gendiff')
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
   .argument('<filepath1>')
