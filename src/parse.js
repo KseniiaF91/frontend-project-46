0ccbdaa8dff8524f4ec83c595d11bfa6d9a01afc
Kseniia Faizulina <ksyushafaizulina@icloud.com>
2023-10-05 20:38:22 +0700

    Add switch to parse.js

--- src/parse.js
+++ src/parse.js
@@ -1,11 +1,16 @@
 import { load } from 'js-yaml';
 
 const parse = (data, fileFormat) => {
-  if (fileFormat === '.json') {
-    return JSON.parse(data);
-  } if (fileFormat === '.yaml' || fileFormat === '.yml') {
-    return load(data);
+  switch (fileFormat) {
+    case '.json':
+      return JSON.parse(data);
+    case '.yaml':
+      return load(data);
+    case '.yml':
+      return load(data);
+    default:
+      throw new Error(`Unknown file format ${fileFormat}`);
   }
-  return console.error(`Unknown file format ${fileFormat}`);
 };
+
 export default parse;
